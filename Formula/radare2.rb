396d82d2ac534582414712b124fdef0a7d9d93c4
Chongyu Zhu <i@lembacon.com>
2019-05-22 20:22:22 +0800

    Update radare2.rb

--- Formula/radare2.rb
+++ Formula/radare2.rb
@@ -1,20 +1,14 @@
 class Radare2 < Formula
   desc "Reverse engineering framework"
   homepage "https://radare.org"
-
-  stable do
-    url "https://radare.mikelloc.com/get/3.5.1/radare2-3.5.1.tar.gz"
-    sha256 "bc927aec4d29fa647fdc56afc2d7b04bf47c2234a43d0080f356705748299d9c"
-  end
+  url "https://radare.mikelloc.com/get/3.5.1/radare2-3.5.1.tar.gz"
+  sha256 "bc927aec4d29fa647fdc56afc2d7b04bf47c2234a43d0080f356705748299d9c"
+  head "https://github.com/radare/radare2.git"
 
   bottle do
     sha256 "2e54078d5cff62cd5593ba2390a0f230e334b94024954e5b6329d52c0401aafc" => :mojave
     sha256 "18dad4749aba2e1d3b5ecb55a1b3c55656bf0e604fc6e739b45a249809053a98" => :high_sierra
     sha256 "67705968143b7235843ad4a494ffca8bbfb886168656a4171bb730dec615a0d2" => :sierra
-  end
-
-  head do
-    url "https://github.com/radare/radare2.git"
   end
 
   def install
